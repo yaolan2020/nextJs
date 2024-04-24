b02e3af43d64a56c50a723803a71dc3c032cf5bf
yaolan <1216827215@qq.com>
2024-04-24 21:45:52 +0800

    feat:chapter:12

---
+++ app/dashboard/invoices/edit-form.tsx
@@ -0,0 +1,18 @@
+// ...
+import { updateInvoice } from '@/app/lib/actions';
+ 
+export default function EditInvoiceForm({
+  invoice,
+  customers,
+}: {
+  invoice: InvoiceForm;
+  customers: CustomerField[];
+}) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+ 
+  return (
+    <form action={updateInvoiceWithId}>
+      <input type="hidden" name="id" value={invoice.id} />
+    </form>
+  );
+}
